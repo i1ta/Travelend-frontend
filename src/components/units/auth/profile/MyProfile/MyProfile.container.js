379c12a091e9a1b844eed3128ef78662775f96e9
seungzzok <ki2684@naver.com>
2023-06-29 02:18:46 +0900

    Docs: 프로필api 연동"

--- src/components/units/auth/profile/MyProfile/MyProfile.container.js
+++ src/components/units/auth/profile/MyProfile/MyProfile.container.js
@@ -1,6 +1,41 @@
-import * as S from './MyProfile.styles'
+import { useEffect, useState } from "react";
 
-export default function MyProfile() {
+import * as S from "./MyProfile.styles";
+import axios from "axios";
+
+export default function MyProfile(props) {
+  // const [data, setData] = useState({
+  //   name: "",
+  //   age: 0,
+  //   email: "",
+  //   gender: "",
+  //   mbti: "",
+  //   phone: "",
+  //   address: "",
+  //   profileUrl: "",
+  //   firstTripStyle: "",
+  //   secondTripStyle: "",
+  //   thirdTripStyle: "",
+  // });
+  // const apiPath = "https://api.tripyle.xyz";
+
+  // useEffect(async () => {
+  //   axios.defaults.headers.common["x-auth-token"] =
+  //     window.localStorage.getItem("login-token");
+  //   await axios
+  //     .get(apiPath + "/profile/my-profile")
+  //     .then((response) => {
+  //       const responseData = { ...response.data };
+  //       setData(responseData);
+  //     })
+  //     .catch((error) => console.error(error));
+  // }, []);
+
+  const formatPhone = (phoneNum) => {
+    const regex = /^(\d{3})(\d{4})(\d{4})$/;
+    return phoneNum.replace(regex, "$1-$2-$3");
+  };
+
   return (
     <>
       <S.MyProfileWrapper>
@@ -9,27 +44,27 @@ export default function MyProfile() {
           <S.Table>
             <tr>
               <S.Tc>이름</S.Tc>
-              <S.Td>홍길동</S.Td>
+              <S.Td>{props.data.name}</S.Td>
               <S.Tc>거주지</S.Tc>
-              <S.Td>서울</S.Td>
+              <S.Td>{props.data.address}</S.Td>
             </tr>
             <tr>
               <S.Tc>나이</S.Tc>
-              <S.Td>30세</S.Td>
+              <S.Td>{props.data.age}</S.Td>
               <S.Tc>선호 인원수</S.Tc>
-              <S.Td>2명</S.Td>
+              <S.Td>null</S.Td>
             </tr>
             <tr>
               <S.Tc>이메일</S.Tc>
-              <S.Td>honggildong@example.com</S.Td>
+              <S.Td>{props.data.email}</S.Td>
               <S.Tc>MBTI</S.Tc>
-              <S.Td>ENFJ</S.Td>
+              <S.Td>{props.data.mbti}</S.Td>
             </tr>
             <tr>
               <S.Tc>연락처</S.Tc>
-              <S.Td>010-1234-5678</S.Td>
+              <S.Td>{formatPhone(props.data.phone)}</S.Td>
               <S.Tc>여행 스타일</S.Tc>
-              <S.Td>관광지</S.Td>
+              <S.Td>{props.data.firstTripStyle}</S.Td>
             </tr>
           </S.Table>
         </S.TableWrapper>
