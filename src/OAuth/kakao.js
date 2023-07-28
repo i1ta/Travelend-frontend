6b1c30db9c2f89411df6dddf6de44dd4ca025fd9
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-07-28 20:38:30 +0900

    Fix: 카카오 로그인 2차 수정

--- src/OAuth/kakao.js
+++ src/OAuth/kakao.js
@@ -1,2 +1,3 @@
 export const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
 export const KAKAO_REDIRECT_URL = "https://tripyle.xyz/oauth/kakao";
+// export const KAKAO_REDIRECT_URL = "http://localhost:3000/oauth/kakao";
