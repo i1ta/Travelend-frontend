f31bc09666836c639b036af8bafc6bf811d38b8d
seungzzok <ki2684@naver.com>
2023-08-04 19:17:06 +0900

    Docs: SNS로그인 수정

--- pages/oauth/kakao.js
+++ pages/oauth/kakao.js
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
-import { useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from "recoil";
 
-import { LoginState, NicknameState } from '@/States/LoginState';
+import { LoginState, NicknameState } from "@/States/LoginState";
 
 const OAuthKaKao = () => {
   // 로그인 상태 설정
@@ -14,39 +14,47 @@ const OAuthKaKao = () => {
   const router = useRouter();
   console.log(router);
   const code = router.query.code;
-  
+
   useEffect(() => {
     if (code) {
-      axios.post(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=9dd98e572c5ca5fb5da7011d9ef2f27f&redirect_uri=https://tripyle.xyz/oauth/kakao&code=${router.query.code}&client_secret=kgyjs7zgBqJ7141qoYq4xqSgOtjdFJKi`,
-      // axios.post(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=9dd98e572c5ca5fb5da7011d9ef2f27f&redirect_uri=http://localhost:3000/oauth/kakao&code=${router.query.code}&client_secret=kgyjs7zgBqJ7141qoYq4xqSgOtjdFJKi`,
-      
-        {
-          "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
-        })
+      axios
+        .post(
+          `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=보안상삭제&redirect_uri=https://tripyle.xyz/oauth/kakao&code=${router.query.code}&client_secret=보안상삭제`,
+
+          {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+          }
+        )
         .then((response) => {
           if (response.status === 200) {
-            axios.post('https://api.tripyle.xyz/user/login/kakao', {
-              // axios.post('http://localhost:3000/user/login/kakao', {
-                "snsId": response.data.id_token,
-                "snsToken": response.data.access_token,
-            }, { "Content-Type": "application/json" })
+            axios
+              .post(
+                "https://api.tripyle.xyz/user/login/kakao",
+                {
+                  snsId: response.data.id_token,
+                  snsToken: response.data.access_token,
+                },
+                { "Content-Type": "application/json" }
+              )
               .then((response2) => {
                 console.log(response2.data);
                 if (response2.status === 200) {
                   setIsLoggedIn(true);
                   setNickname(response2.data.data.nickname);
-                  localStorage.setItem('login-token', response2.data.data.accessToken);
-                  // localStorage.setItem('nickname', response2.data.data.nickname);
-                  if (response2.data.data.needsAdditionalSignUp === true){
-                    router.push('/auth/join/signup');
+                  localStorage.setItem(
+                    "login-token",
+                    response2.data.data.accessToken
+                  );
+                  if (response2.data.data.needsAdditionalSignUp === true) {
+                    router.push("/auth/join/signup");
                   }
-                  router.push('/main');
+                  router.push("/main");
                   setIsLoggedIn(true);
                 }
-            })
+              });
           }
         });
-      }
+    }
   }, [code]);
 
   return <div></div>;
