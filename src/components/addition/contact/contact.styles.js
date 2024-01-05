581976d3b459299a31f29957bdea35e0fe7bde46
seungzzok <ki2684@naver.com>
2024-01-05 12:53:09 +0900

    design: Contact페이  리뉴얼

--- src/components/addition/contact/contact.styles.js
+++ src/components/addition/contact/contact.styles.js
@@ -1,126 +1,107 @@
 import { styled } from "styled-components";
 
 export const MainContainer = styled.div`
-    margin: 100px auto;
+  margin: 40px auto;
+  width: 95%;
+  max-width: 1000px;
 
-    display: flex;
-    flex-direction: column;
-
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const TitleWrapper = styled.div`
-    margin-left: 30px;
+  width: 100%;
 `;
 
 export const MainTitle = styled.div`
-    font-size: 35px;
-    color: #9AB3F5;
-    font-weight: 700;
-
-`;
-
-export const MainTxtBox = styled.div`
-    margin: 40px 0;
+  font-size: 36px;
+  color: ${(props) => props.theme.colors.main1};
+  font-weight: 700;
+  margin-bottom: 24px;
 `;
 
 export const MainTxt = styled.div`
-    font-size: 20px;
-    color: #A7A7A7;
-    margin-top: 20px;
+  font-size: 16px;
+  line-height: 200%;
+  color: #a7a7a7;
+  margin-bottom: 8px;
 `;
 
 export const Line = styled.div`
-    height: 1px;
-    width: 1390px;
-    background-color: #A7A7A7;
-    margin-bottom: 40px;
+  height: 1px;
+  background-color: #a7a7a7;
+  margin-bottom: 40px;
 `;
 
-export const ContentWrapper = styled.div`
-
+export const ContentContainer = styled.div`
+  width: 100%;
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
 `;
 
 export const TitleBox = styled.div`
-    display: flex;
-    
-    width: 1350px;
-    height: 75px;
-    border-radius: 20px;
-    box-shadow: 0px 2px 20px 3px rgba(153, 153, 153, 0.25);
-    margin: 35px 0;
+  display: flex;
+  width: 100%;
+  height: 50px;
+  border-radius: 20px;
+  box-shadow: 0px 2px 20px 3px rgba(153, 153, 153, 0.25);
 `;
 
-export const TitleLittleBox = styled.div`
-    width: 242px;
-    height: 75px;
+export const TitleTag = styled.div`
+  width: 15%;
+  height: 100%;
 
-    text-align: center;
-    align-items: center;
-    line-height: 75px;
-    color: #ffffff;
-    background-color: #C8B6FF;
-    font-size: 25px;
-    border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  background-color: ${(props) => props.theme.colors.main1};
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 10px;
 `;
 
 export const TitleInput = styled.input`
-    width: 900px;
-    margin: 0 auto;
-    font-size: 25px;
-    border: none;
-`;
-
-export const ContentBox = styled.div`
-    width: 1350px;
-    height: 625px;
-
-    text-align: center;
-    line-height: 625px;
-    margin: 30px 0;
-    
-    border: 1px solid #A7A7A7;
-    border-radius: 20px;
+  width: 85%;
+  padding: 0 12px;
+  font-size: 16px;
+  border: none;
+  border-radius: 10px;
 `;
 
 export const ContentInput = styled.textarea`
-    width: 100%;
-    height: 100%;
-    
-    text-align: center;
-    align-items: center;
-    font-size: 30px;
-    border: none;
-    padding: 50px;
-    border-radius: 20px;
-    scroll-y: auto;
-    resize: none;
+  width: 100%;
+  min-height: 400px;
+  font-size: 16px;
+  padding: 20px 16px;
+  border: 1px solid #a7a7a7;
+  border-radius: 10px;
+  resize: none;
 `;
 
 export const PhotoBox = styled.div`
-    width: 1350px;
-    height: 200px;
+  width: 100%;
+  height: 200px;
 
-    text-align: center;
-    line-height: 200px;
-    font-size: 25px;
-    margin: 30px 0;
+  text-align: center;
+  line-height: 200px;
+  font-size: 25px;
 `;
 
-
 export const NoImgWrapper = styled.label`
-    width: 1350px;
-    height: 200px;
+  height: 200px;
 
-  border-radius: 20px;
-  border: 1px solid #A7A7A7;
+  border-radius: 10px;
+  border: 1px solid #a7a7a7;
   background: #fff;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-bottom: 40px;
   position: relative;
   cursor: pointer;
 
@@ -132,7 +113,7 @@ export const NoImgWrapper = styled.label`
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.3);
-    border-radius: 5px;
+    border-radius: 10px;
   }
 
   &:hover::after {
@@ -140,59 +121,34 @@ export const NoImgWrapper = styled.label`
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -55%);
     font-size: 100px;
     color: white;
   }
 `;
 
 export const ImgWrapper = styled.label`
-    overflow: hidden;
-    width: 1350px;
-    height: 200px;
+  overflow: hidden;
+  height: 200px;
+  position: relative;
 
-  border-radius: 20px;
-  border: 1px solid #A7A7A7;
+  border-radius: 10px;
+  border: 1px solid #a7a7a7;
   background: #fff;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  position: relative;
-  cursor: pointer;
-
-  &:hover::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    border-radius: 5px;
-  }
-
-  &:hover::after {
-    content: "x";
-    position: absolute;
-    top: 20%;
-    left: 90%;
-    transform: translate(-50%, -50%);
-    font-size: 100px;
-    color: white;
-  }
 `;
 
 export const Image = styled.img`
-    width: 1350px;
-    height: 200px;
-    object-fit: cover;
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: cover;
 `;
 
 export const NoImgIconWrapper = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 20px;
 `;
 
 export const NoImgIcon = styled.img`
@@ -211,62 +167,48 @@ export const NoImgSubTxt = styled.div`
   font-weight: 500;
 `;
 
-export const EmailBox = styled.div`
-    width: 1350px;
-    height: 75px;
-
-    text-align: center;
-    line-height: 75px;
-    margin: 30px 0;
-    
-    border: 1px solid #A7A7A7;
-    border-radius: 20px;
-`;
-
 export const EmailInput = styled.input`
-    text-align: center;
-    align-items: center;
-    font-size: 25px;
-    border: none;
-    border-radius: 20px;
+  width: 100%;
+  height: 50px;
+  padding: 0 16px;
+
+  border: 1px solid #a7a7a7;
+  border-radius: 10px;
+  font-size: 16px;
 `;
 
 export const EmailNoti = styled.div`
-    font-size: 20px;
-    color: #A7A7A7;
-    margin: 30px 0;
+  width: 100%;
+  font-size: 16px;
+  color: #a7a7a7;
 `;
 
 export const TermsBox = styled.div`
-    display: flex;
-    margin: 30px 0;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
 `;
 
 export const TermsInput = styled.input`
-    width: 24px;
-    height: 24px;
-    border-radius: 5px;
+  width: 20px;
+  height: 20px;
+  border-radius: 5px;
 `;
 
 export const TermsTxt = styled.div`
-    margin-left: 20px;
-    font-size: 20px;
-    color: #A7A7A7;
+  font-size: 16px;
+  color: #a7a7a7;
 `;
 
-export const BtnWrapper = styled.div`
-
-`;
-
-export const Btn = styled.div`
-    width: 1350px;
-    height: 75px;
-    border-radius: 20px;
-    background-color: #C8B6FF;
-    color: #fff;
-    font-size: 25px;
-    font-weight: 700;
-    text-align: center;
-    line-height: 75px;
-    cursor: pointer;
+export const Btn = styled.button`
+  width: 100%;
+  height: 60px;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.colors.main1};
+  color: #fff;
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+  cursor: pointer;
 `;
